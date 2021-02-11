97a508eb40e93f0fffa8eeb0d79d9c9db788b52d
Luigi Domenico <luigidcsoares@gmail.com>
2021-02-11 17:12:44 -0300

    LLVM passes => transformation

--- llvm-passes/lib/AddConstPass.cpp
+++ llvm-passes/lib/AddConstPass.cpp
@@ -29,9 +29,18 @@
 using namespace llvm;
 
 namespace addconst {
+void ReplaceAddInstWithConstant(BinaryOperator *BO) {
+    assert(BO->getOpcode() == Instruction::BinaryOps::Add);
+    auto Fst = cast<ConstantInt>(BO->getOperand(0));
+    auto Snd = cast<ConstantInt>(BO->getOperand(1));
+    auto Sum = ConstantExpr::getAdd(Fst, Snd);
+    BO->replaceAllUsesWith(Sum);
+    BO->eraseFromParent();
+}
+
 PreservedAnalyses AddConstPass::run(Function &F, FunctionAnalysisManager &FAM) {
     auto &AddInsts = FAM.getResult<AddConstAnalysis>(F);
-    // TODO: Evaluate constants and replace uses...
+    for (auto BO : AddInsts) ReplaceAddInstWithConstant(BO);
     auto PA = PreservedAnalyses::all();
     PA.abandon<AddConstAnalysis>();
     return PA;
